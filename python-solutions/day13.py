f581edf146322772545a284fc4fcfec64a82ce9a
Kaj Ström <kaj.strom@kapsi.fi>
2021-11-28 18:51:32 +0200

    Day 13, part 2

--- python-solutions/day13.py
+++ python-solutions/day13.py
@@ -1,5 +1,9 @@
 import sys
+import bisect
+from operator import itemgetter
+
 from utils import time_fn
+import numpy as np
 
 def read_input():
     file = open("inputs/day13.txt", "r")
@@ -7,9 +11,14 @@ def read_input():
     file.close()
 
     buses = []
+    i = 0
     for bus in lines[1].split(","):
         if bus != 'x':
-            buses.append(int(bus))
+            buses.append({
+                "number": int(bus),
+                "index": i
+            })
+        i += 1
 
     return {
         "earliest": int(lines[0]),
@@ -25,6 +34,7 @@ def part1():
 
     earliest = input["earliest"]
     for bus in input["buses"]:
+        bus = bus["number"]
         mod = earliest % bus
         first_possible_depart = earliest + (bus - mod)
 
@@ -40,5 +50,47 @@ def part1():
     print(f"Day 13, part 1: {result}")
 
 
+
+def create_bus_ranges(buses):
+    first_bus_number = buses[0]["number"]
+
+    bus_ranges = []
+    bus_range = range(0, sys.maxsize, first_bus_number)
+    bus_ranges.append(bus_range)
+    for bus in buses[1:]:
+        number = bus["number"]
+        offset = bus["index"] % first_bus_number
+
+        bus_range = range(0, sys.maxsize, number)
+        bus_ranges.append(bus_range)
+
+    return bus_ranges
+
+def part2():
+    buses = input["buses"]
+
+    bus_n = 1
+    for bus in buses:
+        bus_n = bus_n * bus["number"]
+
+    result = 0
+    #Chinese remainder theorem
+    for bus in buses:
+        bi = bus["number"] - bus["index"]
+        mod = bus["number"]
+
+        Ni = int(bus_n / mod)
+
+        xi = pow(Ni, mod - 2, mod)
+
+        temp_res = bi * Ni * xi
+
+        result += temp_res
+
+    result = result % bus_n
+    print(f"Day 13, part 2: {result}")
+
+
 if __name__ == '__main__':
     time_fn(part1)
+    time_fn(part2)
